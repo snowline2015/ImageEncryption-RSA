bc5169a1dd9040bc3545513b32c8b053e3fa9625
HuyHoang19127406 <63275057+HuyHoang19127406@users.noreply.github.com>
2022-01-07 17:36:45 +0700

    login & signup

--- Client/Client.py
+++ Client/Client.py
@@ -1,25 +1,41 @@
 import requests
 import json
 from RSA import RSA_key_generation
-
-url = 'http://127.0.0.1:5000/'
-
-# Get all account
-response = requests.get(url + 'account')
-
-# Get specific account
-response = requests.get(url + 'account/0')
-
-# Register account
-account = {"name": "iamjusthoang","pass": "123","id": "","pub_rsa": ["",""]}
-response = requests.post(url + 'account', json=account)
-
-# Update account
-account = {"name": "iamjusthoang","pass": "123","id": "","pub_rsa": ["",""]}
-response = requests.put(url + 'account/0', json=account)
-
-# Delete account
-response = requests.delete(url + 'account/1')
+from flask import Flask, render_template, url_for, request, redirect
 
 
-print(response.text)
+app = Flask(__name__)
+# url = 'http://127.0.0.1:5000/'
+
+# # Get all account
+# response = requests.get(url + 'account')
+
+# # Get specific account
+# response = requests.get(url + 'account/0')
+
+# # Register account
+# account = {"name": "iamjusthoang","pass": "123","id": "","pub_rsa": ["",""]}
+# response = requests.post(url + 'account', json=account)
+
+# # Update account
+# account = {"name": "iamjusthoang","pass": "123","id": "","pub_rsa": ["",""]}
+# response = requests.put(url + 'account/0', json=account)
+
+# # Delete account
+# response = requests.delete(url + 'account/1')
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
